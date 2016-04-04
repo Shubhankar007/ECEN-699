a731dcc79f54c201b44284f76db19846e07f13b8
shubhankar007 <shubhankar.007@outlook.com>
2016-04-04 00:26:55 +0000

    minisat installed

---
+++ minisat/core/depend.mk
@@ -0,0 +1,27 @@
+/home/ubuntu/workspace/ECEN-699/minisat/core/Main.o /home/ubuntu/workspace/ECEN-699/minisat/core/Main.or /home/ubuntu/workspace/ECEN-699/minisat/core/Main.od /home/ubuntu/workspace/ECEN-699/minisat/core/Main.op: /home/ubuntu/workspace/ECEN-699/minisat/core/Main.cc \
+ /home/ubuntu/workspace/ECEN-699/minisat/utils/System.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/IntTypes.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/utils/ParseUtils.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/utils/Options.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/Vec.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/XAlloc.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/core/Dimacs.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/core/SolverTypes.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/Alg.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/Map.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/Alloc.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/core/Solver.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/Heap.h
+/home/ubuntu/workspace/ECEN-699/minisat/core/Solver.o /home/ubuntu/workspace/ECEN-699/minisat/core/Solver.or /home/ubuntu/workspace/ECEN-699/minisat/core/Solver.od /home/ubuntu/workspace/ECEN-699/minisat/core/Solver.op: /home/ubuntu/workspace/ECEN-699/minisat/core/Solver.cc \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/Sort.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/Vec.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/IntTypes.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/XAlloc.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/core/Solver.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/Heap.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/Alg.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/utils/Options.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/utils/ParseUtils.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/core/SolverTypes.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/Map.h \
+ /home/ubuntu/workspace/ECEN-699/minisat/mtl/Alloc.h
